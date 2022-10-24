867ed53ba93fb21a650151313a14c884d551ace6
Marissa Tobin <marissatobin@192-168-1-102.tpgi.com.au>
2022-10-24 11:34:53 +0800

    reat routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './components/Styles/style.css';
+import { Switch, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Main from './components/main';
 import Footer from './components/Footer'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import CurriculumVitae from './components/CV';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path="/React-Portfolio" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/curriculumvitae" component={CurriculumVitae} />
+        <Route path="/contact" component={Contact} />
+        <Main />
+      </Switch>
       <Footer />
     </div>
   );
 }
-
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,61 +8,61 @@ import travel from './images/Avatar.png';
 
 export default function Portfolio() {
     return (
-        <section class="project">
+        <section className="project">
 			<h1>projects.</h1>
-			<div class="cards">
-				<div class="card">
+			<div className="cards">
+				<div className="card">
 					<a href="https://github.com/marissakrantz/react-travel-app">
-						<img src={travel} alt="Travel App" class="card-image" />
+						<img src={travel} alt="Travel App" className="card-image" />
 					</a>
-						<div class="card-info">
+						<div className="card-info">
 							<h2>Travel App</h2>
-							<div class='tech-repo'>
-								<span class='techs'>React | Mongo | GraphQL</span>
+							<div className='tech-repo'>
+								<span className='techs'>React | Mongo | GraphQL</span>
 								<a href="https://github.com/marissakrantz/react-travel-app">
-									<img src={github} alt='github repo' class='gitRepo'/>
+									<img src={github} alt='github repo' className='gitRepo'/>
 								</a>
 							</div>
 						</div>
 				</div>
-				<div class="card">
+				<div className="card">
 					<a href="https://marissakrantz.github.io/joke-generator/">
-						<img src={joke} alt="Joke Generator project 1" class="card-image" />
+						<img src={joke} alt="Joke Generator project 1" className="card-image" />
 					</a>
-						<div class="card-info">
+						<div className="card-info">
 							<h2>Joke Generator</h2>
-							<div class='tech-repo'>
-								<span class='techs'>Javascript | API</span>
+							<div className='tech-repo'>
+								<span className='techs'>Javascript | API</span>
 								<a href="https://github.com/marissakrantz/joke-generator">
-									<img src={github} alt='github repo' class='gitRepo'/>
+									<img src={github} alt='github repo' className='gitRepo'/>
 								</a>
 							</div>
 						</div>
 				</div>
-				<div class="card">
+				<div className="card">
 					<a href="https://ris-tech-blog.herokuapp.com/">
-						<img src={tech} alt="Tech-Blog" class="card-image" />
+						<img src={tech} alt="Tech-Blog" className="card-image" />
 					</a>
-						<div class="card-info">
+						<div className="card-info">
 							<h2>Tech Blog</h2>
-							<div class='tech-repo'>
-								<span class='techs'>Node | SQL | Handlebars</span>
+							<div className='tech-repo'>
+								<span className='techs'>Node | SQL | Handlebars</span>
 								<a href="https://github.com/marissakrantz/tech-blog">
-									<img src={github} alt='github repo' class='gitRepo'/>
+									<img src={github} alt='github repo' className='gitRepo'/>
 								</a>
 							</div>
 						</div>
 				</div>			
-				<div class="card">
+				<div className="card">
 					<a href="https://marissakrantz.github.io/weather-report/">
-						<img src={weather} alt="Weather-Dashboard" class="card-image" />
+						<img src={weather} alt="Weather-Dashboard" className="card-image" />
 					</a>
-						<div class="card-info">
+						<div className="card-info">
 							<h2>Weather Dashboard</h2>
-							<div class='tech-repo'>
-								<span class='techs'>Javascript | API</span>
+							<div className='tech-repo'>
+								<span className='techs'>Javascript | API</span>
 								<a href="https://github.com/marissakrantz/weather-report">
-									<img src={github} alt='github repo' class='gitRepo'/>
+									<img src={github} alt='github repo' className='gitRepo'/>
 								</a>
 							</div>
 						</div>
